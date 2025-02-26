9e14cf23b0a9cffaa46f8176d46d47007f1a690e
sidram403 <siddharthpatil9108@gmail.com>
2025-02-26 21:25:51 +0530

    change in origin

--- backend/server.js
+++ backend/server.js
@@ -8,27 +8,19 @@ import taskRoutes from './routes/task.route.js'
 import authMiddleware from "./middleware/authMiddleware.js";
 
 dotenv.config();
-const allowedOrigins = [
-  "https://task-management-woad-three.vercel.app/", // Remove trailing slash
-  "http://localhost:5173", // If testing locally
-];
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Allow cookies/auth headers
+    origin: "https://task-management-woad-three.vercel.app",
+    // origin: "http://localhost:5173",
+
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
   })
 );
-app.options("*", cors());
+
 
 // Connect to MongoDB
 mongoose
