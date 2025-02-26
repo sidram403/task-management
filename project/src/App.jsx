a9a8a297c5453182808a839b870f2757fd9f4ae8
sidram403 <siddharthpatil9108@gmail.com>
2025-02-26 12:16:21 +0530

    changed in app.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -21,9 +21,7 @@ const PrivateRoute = ({ children }) => {
 
 const PublicRoute = ({ children }) => {
   const { user, loading } = useAuth();
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+ 
 
   return user ? <Navigate to="/" /> : children; // Redirect to home if authenticated
 };
